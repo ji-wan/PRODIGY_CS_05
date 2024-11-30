fc70cb8358d42754ab51dd72a0deae8d050bbc81
ji-wan <69jiwan@gmail.com>
2024-11-30 21:35:35 +0545

    packet-sniff

--- packetsniffer.py
+++ packetsniffer.py
@@ -10,3 +10,23 @@ def packet_callback(packet):
         
         if packet.haslayer(scapy.TCP):
             try:
+                payload = packet[scapy.Raw].load
+                decode_payload = payload.decode('utf-8', 'ignore')
+                print(f"Payload: {decode_payload}")
+            except(IndexError, UnicodeDecodeError):
+                print("unable to decode TCP payload.")
+            
+        elif packet.haslayer(scapy.UDP):
+            try:
+                payload = packet[scapy.Raw].load
+                decode_payload = payload.decode('utf-8', 'ignore')
+                print(f"Payload: {decode_payload}")
+            except(IndexError, UnicodeDecodeError):
+                print("unable to decode UDP payload.")
+                
+def start_sniffing():
+    scapy.sniff(prn=packet_callback, store=False)
+    
+start_sniffing()
+               
+            
